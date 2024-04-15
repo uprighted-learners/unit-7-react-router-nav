deeea2600ff5cfe7048200067dd376b4d9c40301
Ben Anderson <andersjbe@gmail.com>
2024-04-15 15:40:52 -0400

    Completed Solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
-import './App.css';
+import { BrowserRouter, Link, Route, Router, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
 
 function App() {
   return (
-    <>App</>
+    <div>
+      <BrowserRouter>
+        <ul id="navbar">
+          <li>
+            <Link to={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link to={"/about"}>About</Link>
+          </li>
+          <li>
+            <Link to={"/contact"}>Contact</Link>
+          </li>
+        </ul>
+
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
